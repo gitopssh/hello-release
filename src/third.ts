463ed276af5f5a83c03908ff3c1c6295c4d98d12
Purplown Moonsteer <purplownmstr@users.noreply.github.com>
2023-05-16 13:39:29 +0700

    feat: implement main in third method

--- src/third.ts
+++ src/third.ts
@@ -1,9 +1,12 @@
+import main from './main'
+
 export interface Options {
   name?: string
   age?: number
 }
 
 export const third = (options: Options): void => {
+  main(options)
   console.log('third:', options.name, options.age)
 }
 
